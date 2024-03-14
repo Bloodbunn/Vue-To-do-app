ceb15388530f467e948b3defe4157d21170051cb
Sangar Sdiq Khoshnaw <146385896+Bloodbunn@users.noreply.github.com>
2024-03-14 19:00:10 +0300

    Update TodoList.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,14 +4,14 @@
 
     <div class="flex justify-between w-full  py-1">
         <input v-model="currentTask" @keyup.enter="addTask" type="text" placeholder="Enter task" class="rounded max-[515px]:text-sm mr-2 w-full py-1 px-3 text-xl border border-gray-200 shadow-sm ">
-        <button @click="addTask" class="whitespace-nowrap rounded-md py-1 px-2 bg-green-500 text-white">Add task</button>
+        <button @click="addTask" class="whitespace-nowrap hover:bg-green-600 rounded-md py-1 px-2 bg-green-500 text-white">Add task</button>
 
     </div>
 
     <ul v-for = "(task, index) in tasks" :key="index" class=" w-full py-1 "> 
         <li class="flex justify-between text-center items-center max-[515px]:text-sm border bg-gray-100 rounded-lg border-gray-200 mt-1  text-black px-2 py-2"> 
             {{task}}
-            <button @click="removeTask(index)" class="whitespace-nowrap rounded-md text-sm py-1 px-2 bg-red-500 text-white"> Remove </button>
+            <button @click="removeTask(index)" class="whitespace-nowrap hover:bg-red-700 rounded-md text-sm py-1 px-2 bg-red-500 text-white"> Remove </button>
 
         </li>
     </ul>
